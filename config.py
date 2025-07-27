69f89cf02c625a65eb44c2740d3a42b581bf97ab
kdmcser <zqtndfj@gmail.com>
2025-07-27 23:45:24 +0800

    publish 2.3.0beta5

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-version = "2.3.0beta4"
+version = "2.3.0beta5"
 json_file = "section_list.json"
 install_type_file = "install_type_list.json"
 template_file = "nsis_template"
-output_file = "E:\\vcmi-setup\\vcmi_%s.nsi" % version
-file_dir = "E:\\vcmi-setup\\files"
+output_file = "D:\\vcmi-dev\\vcmi-setup\\vcmi_%s.nsi" % version
+file_dir = "D:\\vcmi-dev\\vcmi-setup\\files"
